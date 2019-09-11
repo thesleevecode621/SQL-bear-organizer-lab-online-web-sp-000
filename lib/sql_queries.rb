e09ede6b95354eca5459442d43529b4d5fda04d8
boruch d <dubywider@gmail.com>
2019-09-11 17:17:15 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament) FROM bears where temperament = 'goofy';"
+  "SELECT COUNT(temperament) FROM bears where temperament = 'goofy';"
   
 end
 
